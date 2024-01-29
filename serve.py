80cf521c94747e0e8503308e149d9f23ccc7a80d
Priyansh Tratiya <77935498+PriyanshTratiya@users.noreply.github.com>
2024-01-28 20:47:24 -0600

    Create serve.py

---
+++ serve.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_ip():
+    hostname = socket.gethostname()
+    private_ip = socket.gethostbyname(hostname)
+    return private_ip
+
+@app.route('/', methods=['POST'])
+def stress_cpu():
+    subprocess.Popen(["python", "stress_cpu.py"])
+    return "CPU stress initiated.", 202
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
